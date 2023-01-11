a10b265d58e765b7f164f326648eb0ad1ae6d783
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-01-11 12:13:23 +0100

    feat(puppet_ssh_config): edit file using puppet

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,16 +1,23 @@
-# Modify client config file
-include stdlib
+# Install puppet standard libs
+exec {'install_lib':
+  path    => '/usr/bin/',
+  command => 'puppet module install puppetlabs-stdlib'
+}
 
-file_line {
-  'off_password_auth':
-    # makes sure the file is available
-    ensure => present,
-    path   => '/etc/ssh/ssh_config',
-    line   => '    PasswordAuthentication no'
-  ;
-  'Declare identity file':
-    # makes sure the file is available
-    ensure => present,
-    path   => '/etc/ssh/ssh_config',
-    line   => '    IdentityFile ~/.ssh/school'
+# make sure the file is available
+file {'/etc/ssh/ssh_config':
+      ensure  => present
+}
+# import stdlib
+include stdlib
+file_line {'off_password_auth':
+    path    => '/etc/ssh/ssh_config',
+    line    => '    PasswordAuthentication no',
+    require => Exec['install_lib']
+}
+
+file_line {'Declare identity file':
+    path    => '/etc/ssh/ssh_config',
+    line    => '    IdentityFile ~/.ssh/school',
+    require => Exec['install_lib']
 }
