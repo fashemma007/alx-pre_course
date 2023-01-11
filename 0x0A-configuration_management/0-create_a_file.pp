7913fbc7bfb3e1bada573314646075b0e39ce920
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-01-11 10:07:54 +0100

    feat(create_a_file): using puppet to create a tmp file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Modify client config file
+$file = "/tmp/school"
+
+#create doc root directory
+file { $file:
+  owner => "www-data",
+  group => "www-data",
+  mode => "0744",
+  content => 'I love Puppet'
+}
