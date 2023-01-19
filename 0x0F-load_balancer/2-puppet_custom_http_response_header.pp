07dc7849077472bde2bdedbda47e46348c4a50d3
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-01-19 13:55:39 +0100

    stye checker

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,32 +1,32 @@
 # sudo apt update -y
-exec { 'Update Apt Repos':
-  command => 'sudo apt update',
-  path    => '/usr/bin/'
+exec { "Update Apt Repos":
+  command => "sudo apt update",
+  path    => "/usr/bin/"
 }
 
 #sudo apt upgrade -y
-exec { 'Run Upgrade':
-  command => 'sudo apt upgrade',
-  path    => '/usr/bin/',
-  require => Exec['Update Apt Repos']
+exec { "Run Upgrade":
+  command => "sudo apt upgrade",
+  path    => "/usr/bin/",
+  require => Exec["Update Apt Repos"]
 }
 # sudo apt install nginx -y
-package { 'nginx':
+package { "nginx":
   ensure  => "installed",
-  require => Exec['Run Upgrade']
+  require => Exec["Run Upgrade"]
 }
 # sed -i "/listen 80 default_server/i add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
 # import stdlib
 include stdlib
-file_line {'append_string':
+file_line {"append_string":
     ensure  => "present",
     path    => "/etc/nginx/sites-available/default",
-    after  => 'listen 80 default_server',
-    line    => 'add_header X-Served-By $HOSTNAME;',
-    require => Package['nginx']
+    after   => "listen 80 default_server",
+    line    => "add_header X-Served-By $HOSTNAME;",
+    require => Package["nginx"]
 }
 # sudo service nginx restart
-service { 'nginx':
+service { "nginx":
   ensure  => running,
-  require => File_line['append_string']
+  require => File_line["append_string"]
 }
