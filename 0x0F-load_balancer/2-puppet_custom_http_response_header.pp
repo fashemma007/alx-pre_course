d471909e5d0b90357be4461524101c68c304628c
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-01-19 13:57:56 +0100

    frustrated approach

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,32 +1,40 @@
-# sudo apt update -y
-exec { "Update Apt Repos":
-  command => "sudo apt update",
-  path    => "/usr/bin/"
+# include stdlib
+# # sudo apt update -y
+# exec { "Update Apt Repos":
+#   command => "sudo apt update",
+#   path    => "/usr/bin/"
+# }
+
+# #sudo apt upgrade -y
+# exec { "Run Upgrade":
+#   command => "sudo apt upgrade",
+#   path    => "/usr/bin/",
+#   require => Exec["Update Apt Repos"]
+# }
+# # sudo apt install nginx -y
+# package { "nginx":
+#   ensure  => "installed",
+#   require => Exec["Run Upgrade"]
+# }
+# # sed -i "/listen 80 default_server/i add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+# file_line {"append_string":
+#     ensure  => "present",
+#     path    => "/etc/nginx/sites-available/default",
+#     after   => "listen 80 default_server",
+#     line    => "add_header X-Served-By $HOSTNAME;",
+#     require => Package["nginx"]
+# }
+# # sudo service nginx restart
+# service { "nginx":
+#   ensure  => running,
+#   require => File_line["append_string"]
+# }
+
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
 
-#sudo apt upgrade -y
-exec { "Run Upgrade":
-  command => "sudo apt upgrade",
-  path    => "/usr/bin/",
-  require => Exec["Update Apt Repos"]
-}
-# sudo apt install nginx -y
-package { "nginx":
-  ensure  => "installed",
-  require => Exec["Run Upgrade"]
-}
-# sed -i "/listen 80 default_server/i add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
-# import stdlib
-include stdlib
-file_line {"append_string":
-    ensure  => "present",
-    path    => "/etc/nginx/sites-available/default",
-    after   => "listen 80 default_server",
-    line    => "add_header X-Served-By $HOSTNAME;",
-    require => Package["nginx"]
-}
-# sudo service nginx restart
-service { "nginx":
-  ensure  => running,
-  require => File_line["append_string"]
-}
