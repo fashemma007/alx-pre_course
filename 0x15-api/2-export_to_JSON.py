ae5087099d5261b30c5d7fce602fa992106789a9
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-02-08 19:03:43 +0100

    feat[to_json]: write API ingested data to json file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Gets employee details"""
 
-from csv import DictWriter, QUOTE_ALL
-import csv
+import json
 from requests import get
 from sys import argv
 
@@ -17,32 +16,18 @@ if __name__ == "__main__":
     user_dits = get(user_url).json()
 
     todos = []
-    todo_list = []
-    hdr_name = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+    json_dict = {}
 
     for todo in user_todo:
         # === writing list of dicts to csv ===
-        todo_dict = {}
-        todo_dict['USER_ID'] = argv[1]
-        todo_dict['USERNAME'] = user_dits.get("name")
-        todo_dict['TASK_COMPLETED_STATUS'] = todo.get("completed")
-        todo_dict['TASK_TITLE'] = todo.get("title")
-        todos.append(todo_dict)
+        user_dict = {}
+        user_dict['task'] = todo.get("title")
+        user_dict['completed'] = todo.get("completed")
+        user_dict['username'] = user_dits.get("username")
+        todos.append(user_dict)
+    json_dict[argv[1]] = todos
 
-        # === writing nested lists to csv ===
-        # tudus = []
-        # tudus.append(argv[1])
-        # tudus.append(user_dits.get("name"))
-        # tudus.append(todo.get("completed"))
-        # tudus.append(todo.get("title"))
-        # todo_list.append(tudus)
-
-    file = "{}.csv".format(argv[1])
-    with open(file, mode="w", newline='') as f:  # open csv file
-        # === writing list of dicts to csv ===
-        writer = DictWriter(f, fieldnames=hdr_name, quoting=QUOTE_ALL)
-        writer.writerows(todos)
-
-        # === writing nested lists to csv ===
-        # writer = csv.writer(f, delimiter=',', quoting=QUOTE_ALL)
-        # writer.writerows(todo_list)
+    file = "{}.json".format(argv[1])
+    with open(file, mode="w", newline='') as f:  # open json file
+        # === writing to json ===
+        json.dump(json_dict, f)
