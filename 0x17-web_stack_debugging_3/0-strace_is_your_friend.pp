754ca884676f9ad634b05ad5528fd314fe7d5e16
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-03-02 21:18:20 +0100

    fix: replace phpp with php

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # identify bug using strace
 $doc_root = '/var/www/html'
+$file_to_edit = '/var/www/html/wp-settings.php'
 
 file { $doc_root:
   ensure => 'directory',
@@ -13,4 +14,9 @@ file { "${doc_root}/index.html":
         path    => '/var/www/html/index.html',
         content => '<h1>Bug Fixed by fash the great</h1>',
         require => File[$doc_root]
+}
+#replace line containing "phpp" with "php"
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
