3a44c4a59a4111101cc6780b3bbffc2d4995c5fc
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-03-02 20:51:17 +0100

    feat[strace]: debug apache errors using strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,15 @@
+$doc_root = '/var/www/html'
+
+file { $doc_root:
+  ensure => 'directory',
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode   => '0644'
+}
+
+file { "${doc_root}/index.html":
+        ensure  => 'present',
+        path    => '/var/www/html/index.html',
+        content => '<h1>Bug Fixed by fash the great</h1>',
+        require => File[$doc_root]
+}
